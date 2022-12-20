cbcc202288e57bc9504099c3b1f90f0157c7d038
Hiroshi Nishio <4620828+hiroshinishio@users.noreply.github.com>
2022-12-20 19:08:54 +0900

    Removed an unused custome route

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,3 +1,5 @@
+// /slack/events is the default endpoint for the Slack Events API
+// You can add other endpoints other that /slack/events
 const routes = [
   {
     path: "/health", // To check if the app is running
@@ -6,14 +8,6 @@ const routes = [
     handler: (_req, res) => {
       res.writeHead(200);
       res.end("Server running " + new Date().toISOString() );
-    },
-  },
-  {
-    path: "/slack/events", // The default path for listening to Slack events
-    method: ["POST"], // Must be POST
-    // @ts-ignore
-    handler: (_req, res) => {
-      res.writeHead(200);
     },
   },
 ];
